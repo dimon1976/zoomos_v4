02637a72e53fed70e0849bd4067a23b6ef790872
Dimon <malinovkadom@gmail.com>
2025-08-13 23:28:47 +0300

    fix

--- src/main/java/com/java/service/exports/ExportProgressService.java
+++ src/main/java/com/java/service/exports/ExportProgressService.java
@@ -9,6 +9,7 @@ import com.java.repository.FileOperationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
