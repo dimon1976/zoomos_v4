54d26cdaab61cb98500e21540faa5d3a324950a0
Dimon <malinovkadom@gmail.com>
2026-07-02 01:08:49 +0300

    fix(report-fetcher): eager-fetch client association on ReportConfig list queries to avoid LazyInitializationException in templates

--- src/main/java/com/java/repository/ReportConfigRepository.java
+++ src/main/java/com/java/repository/ReportConfigRepository.java
@@ -1,6 +1,7 @@
 package com.java.repository;
 
 import com.java.model.entity.ReportConfig;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,9 @@ import java.util.List;
 @Repository
 public interface ReportConfigRepository extends JpaRepository<ReportConfig, Long> {
 
+    @EntityGraph(attributePaths = "client")
     List<ReportConfig> findAllByOrderByNameAsc();
 
+    @EntityGraph(attributePaths = "client")
     List<ReportConfig> findAllByClientIdOrderByNameAsc(Long clientId);
 }
